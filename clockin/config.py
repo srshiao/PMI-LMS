32fc58056f6b2fa2d0e5c566e414544c347b5a5e
Sam Shiao <srshiao@gmail.com>
2018-01-03 18:28:50 -0500

    Removed config.py, added gitignore

--- clockin/config.py
+++
@@ -1,8 +0,0 @@
-SLACK_BOT_TOKEN = "xoxp-188174792402-237245353986-255525795463-1d610c1593b646ad22aba14a8ea59c47"
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'PMIClockin@gmail.com'
-EMAIL_HOST_PASSWORD = 'paradyme1'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-PERIOD = "Spring 2018"
